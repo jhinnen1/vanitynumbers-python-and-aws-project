920d024fc2a54f4df266e882d21cfc66a3a26e46
Jerimiah W. Hinnen <70825145+jhinnen1@users.noreply.github.com>
2021-11-17 18:01:38 -0600

    Final copy of lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,42 @@
+import json
+import boto3
+import vanitynumber
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('vanitynumbers_results')
+
+def lambda_handler(event, context):
+    inbound_number = event\
+        .get("Details", {})\
+        .get("ContactData", {})\
+        .get("CustomerEndpoint", {})\
+        .get("Address", None)
+        
+    def fix_phone_number():
+	    x = inbound_number
+	    return('{}-{}-{}-{}'.format(x[1],x[2:5],x[5:8],x[8:]))
+
+    caller_number = fix_phone_number()
+    
+    vanity_numbers = vanitynumber.all_wordifications(caller_number)
+    
+    
+
+    table.put_item(
+        Item={
+            'caller phone number': caller_number,
+            'vanity numbers': vanity_numbers
+        }
+    )    
+    
+    
+    resultMap = {"caller phone number":" ","vanity numbers":" "}
+    return resultMap
+    
+    response = {
+        'message': 'Item added'
+    }
+    return {
+        'statusCode': 200,
+        'body': response
+    }
